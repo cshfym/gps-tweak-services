c028c9df93360d243b4f519edbc3d15cb2dcb9ca
cshfym <cshfym@gmail.com>
2014-12-15 11:24:48 -0700

    Disable database session.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -50,7 +50,7 @@ grails.project.dependency.resolution = {
 
         build ":tomcat:$grailsVersion"
         runtime ":database-migration:1.1"
-        complie ":database-session:1.2.1" // Sticky sessions.
+        // complie ":database-session:1.2.1" // Sticky sessions.
         compile ":heroku:1.0.1"
         compile ':cache:1.0.0'
         compile ':cloud-support:1.0.11'
